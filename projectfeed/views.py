2e9a870bc3ab9cbb6d44e1e934c5a0f9a3d6df42
Michael Kosmicki <michaelkosmicki@yahoo.com>
2019-01-10 21:19:30 -0500

    Installed and modified bootstrap theme

--- projectfeed/views.py
+++ projectfeed/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Feed Home</h1>')
+    return render(request, 'projectfeed/home.html')
 
 def about(request):
-    return HttpResponse('<h1>About Me</h1>')
+    return render(request, 'projectfeed/about.html')
